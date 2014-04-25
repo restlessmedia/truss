2ba3d8e3acf69d8343ecb90c6b6828edc797b4e9
Phil Cooper <phillycooper@hotmail.com>
2014-04-25 15:22:05 +0100

    Removed plugin examples

--- Examples/formatDate.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    var plugin = function (container, field, value) {
-        console.log(arguments);
-        $(field.element).text(value.getFullYear() + '.' + value.getMonth());
-    };
-    truss.registerFieldPlugin('formatDate', plugin);
-}).call(this);
