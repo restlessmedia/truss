b58207a355933f431fca0d95337aa13d98a1fef8
Phil Cooper <phillycooper@hotmail.com>
2014-04-29 08:51:30 +0100

    Removing auto bind and events

--- truss.js
+++ truss.js
@@ -1,189 +1,178 @@
 (function () {
-    var _root = this;
-    var _truss;
-    var _plugins = [];
-    var _displayStates = {none: false, block: true, inline: true};
+  var _root = this;
+  var _truss;
+  var _plugins = [];
+  var _displayStates = { none: false, block: true, inline: true };
 
-    if (typeof exports !== 'undefined') {
-        _truss = exports;
-    } else {
-        _truss = _root.truss = {};
+  if (typeof exports !== 'undefined') {
+    _truss = exports;
+  } else {
+    _truss = _root.truss = {};
+  }
+
+  _truss.version = '1.0.1';
+  _truss.$ = _root.jQuery || _root.$;
+  _root.truss = _truss;
+
+  _truss.registerPlugin = function registerPlugin(name, plugin) {
+    _plugins[name] = plugin;
+  };
+
+  var attr = function (element, name, value) {
+    if (arguments.length === 3) {
+      element.setAttribute(name, value);
+    } else if (element.hasAttribute(name)) {
+      value = element.getAttribute(name);
     }
+    return value;
+  };
 
-    _truss.version = '1.0.1';
-    _truss.$ = _root.jQuery || _root.$;
-    _root.truss = _truss;
-
-    _truss.registerPlugin = function registerPlugin(name, plugin) {
-        _plugins[name] = plugin;
-    };
-
-    var attr = function (element, name, value) {
-        if (arguments.length === 3) {
-            element.setAttribute(name, value);
-        } else if (element.hasAttribute(name)) {
-            value = element.getAttribute(name);
+  var forEach = function (data, fn) {
+    if (!Array.prototype.forEach) {
+      var length = data.length >>> 0;
+      while (++i < length) {
+        if (i in data) {
+          fn.apply(data[i], [data[i], i]);
         }
-        return value;
-    };
+      }
+    } else {
+      data.forEach(fn);
+    }
+  };
 
-    var forEach = function (data, fn) {
-        if (!Array.prototype.forEach) {
-            var length = data.length >>> 0;
-            while (++i < length) {
-                if (i in data) {
-                    fn.apply(data[i], [data[i], i]);
-                }
-            }
-        } else {
-            data.forEach(fn);
-        }
-    };
+  var visible = function (element, isVisible) {
+    if (arguments.length === 1) {
+      return element.style.display ? _displayStates[element.style.display.toLowerCase()] : true;
+    }
+    element.style.display = isVisible ? 'block' : 'none';
+    return isVisible;
+  };
 
-    var visible = function (element, isVisible) {
-        if (arguments.length === 1) {
-            return element.style.display ? _displayStates[element.style.display.toLowerCase()] : true;
-        }
-        element.style.display = isVisible ? 'block' : 'none';
-        return isVisible;
-    };
+  var data = function (element) {
+    return $(element).data();
+  };
 
-    var data = function (element) {
-        return $(element).data();
-    };
+  var text = function (element, value) {
+    if (element.childNodes.length) {
+      if (element.firstChild.nodeType === 3) {
+        element.firstChild.nodeValue = value;
+      }
+    } else {
+      element.appendChild(document.createTextNode(value));
+    }
+  };
 
-    var text = function (element, value) {
-        if (element.childNodes.length) {
-            if (element.firstChild.nodeType === 3) {
-                element.firstChild.nodeValue = value;
-            }
-        } else {
-            element.appendChild(document.createTextNode(value));
-        }
-    };
+  var fetch = function (container) {
+    return $.ajax({ url: container.src });
+  };
 
-    var fetch = function (container) {
-        return $.ajax({ url: container.src });
-    };
+  var resolve = function (data, reference) {
+    var parts = reference.split('.');
+    var length = parts.length;
 
-    var resolve = function (data, reference) {
-        var index = function (d, name, i) {
-            return d[name];
-        };
-        return reference.split('.').reduce(index, data);
+    var index = function (d, name, i) {
+      return d[name];
     };
+    return reference.split('.').reduce(index, data);
+  };
 
-    var Container = function (element, config) {
-        this.src = config.src;
-        this.element = element;
-        this.autoBind = attr(element, 'data-bind') !== 'false';
-        return this;
-    };
+  var Container = function (element, config) {
+    this.src = config.src;
+    this.element = element;
+    return this;
+  };
 
-    Container.prototype.raiseEvent = function (name, args) {
-        args = args || [];
-        args.unshift(this); // insert container into beginning of args
-        $(this.element).trigger(name, args);
-    };
+  Container.prototype.getValue = function (element, data, name) {
+    var value = resolve(data, name);
+    var pluginName = attr(element, 'data-plugin');
+    if (pluginName && _plugins[pluginName]) {
+      value = _plugins[pluginName].call(this, element, value);
+    }
+    return value;
+  };
 
-    Container.prototype.getValue = function (element, data, name) {
-        var value = resolve(data, name);
-        var pluginName = attr(element, 'data-plugin');
-        if (pluginName && _plugins[pluginName]) {
-            value = _plugins[pluginName].call(this, element, value);
-        }
-        return value;
-    };
+  Container.prototype.setValue = function (element, value) {
+    var format = attr(element, 'data-format');
+    if (format) {
+      value = format.replace(/\$s/g, value)
+    }
+    switch (element.tagName.toLowerCase()) {
+      case 'img':
+        attr(element, 'src', value);
+        break;
+      default:
+        text(element, value);
+    }
+  };
 
-    Container.prototype.setValue = function (element, value) {
-        var format = attr(element, 'data-format');
-        if (format) {
-            value = format.replace(/\$s/g, value)
-        }
-        switch (element.tagName.toLowerCase()) {
-            case 'img':
-                attr(element, 'src', value);
-                break;
-            default:
-                text(element, value);
-        }
-    };
+  Container.prototype.bindField = function (element, data) {
+    var name = attr(element, 'data-name');
+    if (name) {
+      var value = this.getValue(element, data, name);
+      if (value) {
+        this.setValue(element, value);
+      }
+      else {
+        $(element).hide();
+      }
+    }
+  };
 
-    Container.prototype.bindField = function (element, data) {
-        var name = attr(element, 'data-name');
-        if (name) {
-            var value = this.getValue(element, data, name);
-            if (value) {
-                this.setValue(element, value);
-            }
-            else {
-                $(element).hide();
-            }
-        }
-    };
-
-    Container.prototype.bindRow = function (row, data) {
-        var that = this;
-        this.bindField(row, data);
-        $(row).find('[data-name]').each(function () {
-            that.bindField(this, data);
-        });
-    };
-
-    Container.prototype.addRow = function (data, after) {
-        var row = this.element.cloneNode(true);
-        // remove src for all new rows
-        row.removeAttribute('data-src');
-        this.bindRow(row, data);
-        this.element.parentNode.insertBefore(row, after.nextSibling);
-        return row;
-    };
-
-    Container.prototype.bind = function () {
-        var that = this;
-        fetch(this).done(function (data) {
-            // always work with a collection
-            data = 'length' in data ? data : [data];
-            if (data.length) {
-                // get the first record, this is applied to the first template row
-                var first = data.shift();
-                // loop through remaining records
-                var row;
-                forEach(data, function (obj, i) {
-                    row = that.addRow(obj, /* after: previous row or container element */ row || that.element);
-                });
-                // bind to the template row
-                that.bindRow(that.element, first);
-            } else {
-                // no data, hide the entire container element
-                visible(that.element, false);
-            }
-            that.raiseEvent('bound');
-        }).fail(function () {
-            visible(that.element, false);
-            that.raiseEvent('fetchFailed');
-            console.log(arguments);
-        });
-    };
-
-    var init = function (container) {
-        if (container.autoBind) {
-            container.bind();
-        }
-        $(container.element).on('bind', function () {
-            container.bind();
-        });
-    };
-
-    var findContainers = function () {
-        var containers = [];
-        _truss.$('[data-src]').each(function () {
-            containers.push(new Container(this, data(this)));
-        });
-        return containers;
-    };
-
-    $(document).ready(function () {
-        forEach(findContainers(), init);
+  Container.prototype.bindRow = function (row, data) {
+    var that = this;
+    this.bindField(row, data);
+    $(row).find('[data-name]').each(function () {
+      that.bindField(this, data);
     });
+  };
+
+  Container.prototype.addRow = function (data, after) {
+    var row = this.element.cloneNode(true);
+    // remove src for all new rows
+    row.removeAttribute('data-src');
+    this.bindRow(row, data);
+    this.element.parentNode.insertBefore(row, after.nextSibling);
+    return row;
+  };
+
+  Container.prototype.bind = function () {
+    var that = this;
+    fetch(this).done(function (data) {
+      // always work with a collection
+      data = 'length' in data ? data : [data];
+      if (data.length) {
+        // get the first record, this is applied to the first template row
+        var first = data.shift();
+        // loop through remaining records
+        var row;
+        forEach(data, function (obj, i) {
+          row = that.addRow(obj, /* after: previous row or container element */ row || that.element);
+        });
+        // bind to the template row
+        that.bindRow(that.element, first);
+      } else {
+        // no data, hide the entire container element
+        visible(that.element, false);
+      }
+    }).fail(function () {
+      visible(that.element, false);
+      console.log(arguments);
+    });
+  };
+
+  var init = function (container) {
+    container.bind();
+  };
+
+  var findContainers = function () {
+    var containers = [];
+    _truss.$('[data-src]').each(function () {
+      containers.push(new Container(this, data(this)));
+    });
+    return containers;
+  };
+
+  $(document).ready(function () {
+    forEach(findContainers(), init);
+  });
 }).call(this);
